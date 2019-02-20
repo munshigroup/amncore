0c8f6cc498c02cff1db2721b883c822e70fd4c5e
Sumit Munshi <46409920+smunshi@users.noreply.github.com>
2019-02-19 23:06:40 -0500

    pypiwin32 req added for win32 installs

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
+import re
 import setuptools
+import sys
 
 with open("README.md", "r") as fd:
     long_description = fd.read()
 
+prereqs = ['bombfuse', 'kthread']
+
+if re.search(r'win(32|64)', sys.platform) is not None:
+    prereqs.append('pypiwin32')
+    
 setuptools.setup(
     name="amncore",
-    version="0.1.2",
+    version="0.1.3",
     author="The Munshi Group",
     author_email="support@munshigroup.com",
     description="AmmuNation Framework core modules",
@@ -13,7 +20,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/munshigroup/amncore",
     packages=setuptools.find_packages(),
-    install_requires = ['bombfuse', 'kthread'],
+    install_requires = prereqs,
     classifiers=[
         "Programming Language :: Python :: 2",
         "License :: OSI Approved :: MIT License",
